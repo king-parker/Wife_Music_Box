8ad67f6afd594fc735a4b0843944893f5ef9bf4b
Parker King <kingparker.a@gmail.com>
2022-12-13 18:20:29 -0700

    Update basic prototype to be triggerable

--- Prototype_Sketches/TriggeredFade/TriggeredFade.ino
+++ Prototype_Sketches/TriggeredFade/TriggeredFade.ino
@@ -1,19 +1,12 @@
 /*
-  Fade
+  Triggered Fade
 
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
+  This code lights up an led that will fade in and out while audio is played from an
+  Adafruit Soundboard.
 */
 
 int led = 9;         // the PWM pin the LED is attached to
+int soundActive = 22;
 int brightness = 0;  // how bright the LED is
 int fadeAmount = 5;  // how many points to fade the LED by
 
@@ -21,12 +14,19 @@ int fadeAmount = 5;  // how many points to fade the LED by
 void setup() {
   // declare pin 9 to be an output:
   pinMode(led, OUTPUT);
+  pinMode(soundActive, INPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
-  analogWrite(led, brightness);
+  // Adafruit Soundboard active pin outputs low when audio is playing
+  if (digitalRead(soundActive)){
+    // set the brightness of pin 9:
+    analogWrite(led, 0);
+  }
+  else {
+    analogWrite(led, brightness);
+  }
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
